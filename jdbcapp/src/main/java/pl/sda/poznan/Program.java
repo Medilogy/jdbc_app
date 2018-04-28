f68b61181ab19b09d27338f54d686ae9f13ca02e
Michal Madajewski <mw.madajewski@gmail.com>
2018-04-28 15:17:36 +0200

    add tables with data

--- jdbcapp/src/main/java/pl/sda/poznan/Program.java
+++ jdbcapp/src/main/java/pl/sda/poznan/Program.java
@@ -1,9 +1,26 @@
 package pl.sda.poznan;
 
-public class Program {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Program extends Application {
+
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load
+                (getClass()
+                        .getClassLoader()
+                        .getResource("view/MainWindow.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
-
-
+        launch(args);
     }
+
 }
+
---
+++ jdbcapp/src/main/java/pl/sda/poznan/controller/MainWindowController.java
@@ -0,0 +1,9 @@
+package pl.sda.poznan.controller;
+
+import javafx.event.ActionEvent;
+
+public class MainWindowController {
+    public void handleClick(ActionEvent actionEvent) {
+        System.out.println("Button clicked");
+    }
+}
